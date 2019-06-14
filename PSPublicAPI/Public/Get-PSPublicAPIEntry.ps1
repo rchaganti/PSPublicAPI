6dc748c5b51f3086a05e3c9b9453006c5fc1aa04
rchaganti <ravikanth@ravichaganti.com>
2019-06-14 09:15:51 +0530

    Initial commit

---
+++ PSPublicAPI/Public/Get-PSPublicAPIEntry.ps1
@@ -0,0 +1,35 @@
+﻿function Get-PSPublicAPIEntry
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [String]
+        $Title,
+
+        [Parameter()]
+        [String]
+        $Description,
+
+        [Parameter()]
+        [String]
+        $Authentication,
+
+        [Parameter()]
+        [Switch]
+        $HttpsOnly,
+
+        [Parameter()]
+        [ValidateSet('yes','no','unknown')]
+        [String]
+        $Cors,
+
+        [Parameter()]
+        [Switch]
+        $Category
+    )
+
+    $uri = "${baseUri}entries"
+    $entries = (Invoke-RestMethod -Method Get -UseBasicParsing -Uri $uri).Entries
+    return $entries
+}
